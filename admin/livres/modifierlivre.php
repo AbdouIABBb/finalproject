07be3c70db6d46720680b0ea7e836e975af0078b
C00LD3R <aomar.khelifi2001@gmail.com>
2022-06-01 17:26:29 +0100

    modifier resume livre

--- admin/livres/modifierlivre.php
+++ admin/livres/modifierlivre.php
@@ -9,7 +9,9 @@ $image = $_FILES['image'];
 $quantite = $_POST['quantite'];
 $category = $_POST['category'];
 $auteur = $_POST['auteur'];
+$resume = $_POST['resume'];
 if(empty($nom)) $nom = $book['nom'];
+if(empty($resume)) $resume = $book['resume'];
 if(empty($prix)) $prix = $book['prix'];
 if(empty($image['name'])){
     $image = $book['image'];
@@ -20,7 +22,7 @@ if(empty($quantite)) $quantite = $book['quantite'];
 if(empty($category)) $category = $book['category'];
 if(empty($auteur)) $auteur = $book['auteur'];
 $conn = connect();
-$requette = "UPDATE book SET nom = '$nom', prix = '$prix', image = '$image', quantite = $quantite, category = '$category', auteur = '$auteur'WHERE id= '$id' ";
+$requette = "UPDATE book SET nom = '$nom', resume = '$resume' , prix = '$prix', image = '$image', quantite = $quantite, category = '$category', auteur = '$auteur'WHERE id= '$id' ";
 $resultat = $conn ->query($requette);
 
 if ($resultat){

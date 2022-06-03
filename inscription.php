4c379f89bdf4ab4214ef73c58365438b8a80e9f7
raniamasdoua <masdouarania02@gmail.com>
2022-06-04 00:09:30 +0100

    login register

--- inscription.php
+++ inscription.php
@@ -61,7 +61,7 @@ if (!empty($_POST)){
             <button type="submit" class="btn btn-primary">Confirmer</button>
         </form>
     </div>
-    <?php  include"inc/footer.php"?>
+    
   </body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.6/sweetalert2.all.min.js"></script>

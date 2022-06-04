279bff65b7e0a758c9387d84cc20e1755daa05c2
C00LD3R <aomar.khelifi2001@gmail.com>
2022-06-04 12:30:10 +0100

    multiple books in one command problem

--- actions/valider-panier.php
+++ actions/valider-panier.php
@@ -12,7 +12,7 @@ include"../inc/functions.php" ;
  $ville=$_POST['ville'];
  
 
-//creation de panier
+// creation de panier
 $requette_panier ="INSERT INTO panier(user,total,date_creation, rue, ville ) VALUES('$user','$total','$date', '$rue', '$ville') ";
 $resultat = $conn ->query($requette_panier);
 $panier_id = $conn ->LastInsertId();
@@ -21,20 +21,28 @@ $commandes=$_SESSION['panier'][3];
  foreach($commandes as $commande)
  {
 
-// //Ajouter commande
+//Ajouter commande
     $quantite=$commande[0];
     $total=$commande[1];
     $id_produit=$commande[4];
-    $new = $book['quantite'] - $quantite;
     $rue= $_POST['rue'];
     $requette = "INSERT INTO  commandes(quantite,total,panier,date_creation,produit) VALUES('$quantite','$total','$panier_id','$date','$id_produit')";
     $resultat=$conn ->query($requette);
+
+ }
+
+ foreach($commandes as $commande)
+ {
+    $id_produit=$commande[4];
+    $theBook = getbookbyid($id_produit);
+    $quantite=$commande[0];
+    $new = $theBook['quantite'] - $quantite;
 
     $requette = "UPDATE book SET quantite = '$new' WHERE id = '$id_produit'";
     $resultat=$conn ->query($requette);
 
  }
-//supprimer le panier
+// supprimer le panier
 unset($_SESSION['bookid']);
 $_SESSION['panier']=null;
 header('location:../index.php')
